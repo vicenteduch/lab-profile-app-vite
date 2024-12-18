17cad658da98c68d517a214e78ccedf4189d6891
Vicente Duch <vhduch@gmail.com>
2024-12-18 12:24:50 +0100

    Initial commit

--- Lab-profile-app-vite/routes/index.routes.js
+++ Lab-profile-app-vite/routes/index.routes.js
@@ -1,4 +1,5 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
